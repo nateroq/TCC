76e264c00a08ba72f21ce96d726320a6be409d45
I452652 <n.moreiraroque@student.fontys.nl>
2021-11-08 00:03:11 -0300

    Arquivo final com resultados

--- TCC.lua
+++ TCC.lua
@@ -1,58 +1,95 @@
 --#region Flags and Globals
 -- Parameters that can be changed
-experiment_id = 1; -- flag for experiment
-max_number_replications = 2 -- total number of replications for experiment
-navigation_file = 'Missions_TCC/way_1.txt';
-param:set('SIM_WIND_SPD',0);
+experiment_id = 9; -- flag for experiment
+max_number_replications = 10; -- total number of replications for experiment
+navigation_file = 'Missions_TCC/openangles.waypoints'; --mission file
+param:set('WPNAV_SPEED',1000); -- Horizontal speed
+param:set('WPNAV_ACCEL',250); -- Horizontal acceleration
+param:set('SIM_WIND_SPD',0); -- Horizontal Wind speed
 flag_ctrl_mission = 1;
 -- Global Variables that should not be changed
 replication_number = 0; -- replication number of the experiment
-repeat_mission = true
-mission_loaded = false
-mission_started = false
-mission_ended = true
+repeat_mission = true; -- bool flag for mission repetition
+mission_loaded = false; -- bool flag for mission load
+mission_started = false; -- bool flag to star mission
+mission_ended = true; -- bool flag to end mission
 wp = mission:get_current_nav_index(); -- flag for wi=wi+1 control
-vetor_erro = {0,0,0}; -- error vector
-verror_size = 0;
+vetor_erro = {0,0,0}; -- mear error vector
+verror_size = 0; -- size of mean error vector
 written_file_replication = false;
+first_position = 0;
 --#endregion
 
 function experiment_control() -- function that controls the creation of csv for the whole experiment
   mean_file_name = "logs/exp/exp" .. tostring(experiment_id) .. "_mean.csv"; --file name changes for each experiment
   mean_file = io.open(mean_file_name, "a"); --append/create .csv
-  mean_file:write('Replication, Ex(m), Ey(m), Ez(m)\n') --csv headers
-  mean_file:flush() --save to file
+  mean_file:write('Replication, Ex(m), Ey(m), Ez(m)\n'); --csv headers
+  mean_file:flush(); --save to file
 
   all_errors_file_name = "logs/exp/exp" .. tostring(experiment_id) .. "_all_errors.csv"; --file name changes for each experiment
   all_errors_file = io.open(all_errors_file_name, "a"); --append/create .csv
-  all_errors_file:write('Ex(m), Ey(m), Ez(m)\n') --csv headers
-  all_errors_file:flush() --save to file
+  all_errors_file:write('Ex(m), Ey(m), Ez(m)\n'); --csv headers
+  all_errors_file:flush(); --save to file
 end
 
 --#region Setting parameters for missions
 function mission_param()
   if experiment_id == 2 then
     param:set('SIM_WIND_SPD',5);
+    param:set('WPNAV_SPEED',1000);
+    param:set('WPNAV_ACCEL',250);
   end
   if experiment_id == 3 then
     param:set('SIM_WIND_SPD',10);
+    param:set('WPNAV_SPEED',1000);
+    param:set('WPNAV_ACCEL',250);
   end
   if experiment_id == 4 then
     navigation_file = 'Missions_TCC/closedangles.waypoints';
     param:set('SIM_WIND_SPD',5);
+    param:set('WPNAV_SPEED',1000);
+    param:set('WPNAV_ACCEL',250);
   end
   if experiment_id == 5 then
     navigation_file = 'Missions_TCC/smallsquare.waypoints';
     param:set('SIM_WIND_SPD',5);
-    repeat_mission = false
+    param:set('WPNAV_SPEED',1000);
+    param:set('WPNAV_ACCEL',250);
   end
+  if experiment_id == 6 then
+    param:set('SIM_WIND_SPD',5);
+    param:set('WPNAV_SPEED',1000);
+    param:set('WPNAV_ACCEL',125);
+  end
+  if experiment_id == 7 then
+    param:set('SIM_WIND_SPD',5);
+    param:set('WPNAV_SPEED',1000);
+    param:set('WPNAV_ACCEL',300);
+  end
+  if experiment_id == 8 then
+    param:set('SIM_WIND_SPD',5);
+    param:set('WPNAV_SPEED',500);
+    param:set('WPNAV_ACCEL',250);
+  end
+  if experiment_id == 9 then
+    param:set('SIM_WIND_SPD',5);
+    param:set('WPNAV_SPEED',1500);
+    param:set('WPNAV_ACCEL',250);
+  end
+  local param1 = param:get('SIM_WIND_SPD');
+  local param2 = param:get('WPNAV_SPEED');
+  local param3 = param:get('WPNAV_ACCEL');
+  gcs:send_text(6, string.format('Wind:  %i',param1));
+  gcs:send_text(6, string.format('Speed: %i',param2));
+  gcs:send_text(6, string.format('Accel: %i',param3));
+
 end
 --#endregion
 
 --#region WRITE DATA TO OPEN FILE
 function write_to_file(file, interesting_data)
   if not file then
-    error("Could not open file")
+    error("Could not open file");
   end
   -- separate with comas and add a carriage return
   -- make sure file is upto date
@@ -65,49 +102,49 @@ end
 --#region READ MISSION
 function read_mission(mission_file_name)
   -- Open file
-  mission_file = assert(io.open(mission_file_name), 'Could open :' .. mission_file_name)
+  mission_file = assert(io.open(mission_file_name), 'Could open :' .. mission_file_name);
 
   -- check header
-  assert(string.find(mission_file:read('l'),'QGC WPL 110') == 1, mission_file_name .. ': incorrect format')
+  assert(string.find(mission_file:read('l'),'QGC WPL 110') == 1, mission_file_name .. ': incorrect format');
 
   -- clear any existing mission
-  assert(mission:clear(), 'Could not clear current mission')
+  assert(mission:clear(), 'Could not clear current mission');
 
   -- read each line and write to mission
-  local item = mavlink_mission_item_int_t()
-  local index = 0
+  local item = mavlink_mission_item_int_t();
+  local index = 0;
   while true do
 
-    local data = {}
+    local data = {};
     for i = 1, 12 do
-      data[i] = mission_file:read('n')
+      data[i] = mission_file:read('n');
       if data[i] == nil then
         if i == 1 then
-          gcs:send_text(6, 'loaded mission: ' .. mission_file_name)
+          gcs:send_text(6, 'loaded mission: ' .. mission_file_name);
           return -- got to the end of the file
         else
           mission:clear() -- clear part loaded mission
-          error('failed to read file')
+          error('failed to read file');
         end
       end
     end
 
-    item:seq(data[1])
-    item:frame(data[3])
-    item:command(data[4])
-    item:param1(data[5])
-    item:param2(data[6])
-    item:param3(data[7])
-    item:param4(data[8])
-    item:x(data[9]*10^7)
-    item:y(data[10]*10^7)
-    item:z(data[11])
+    item:seq(data[1]);
+    item:frame(data[3]);
+    item:command(data[4]);
+    item:param1(data[5]);
+    item:param2(data[6]);
+    item:param3(data[7]);
+    item:param4(data[8]);
+    item:x(data[9]*10^7);
+    item:y(data[10]*10^7);
+    item:z(data[11]);
 
     if not mission:set_item(index,item) then
-      mission:clear() -- clear part loaded mission
-      error(string.format('failed to set mission item %i',index))
+      mission:clear(); -- clear part loaded mission
+      error(string.format('failed to set mission item %i',index));
     end
-    index = index + 1
+    index = index + 1;
   end
 
 end
@@ -119,7 +156,7 @@ function MvV(m1,m2)
   for i = 1, 3, 1 do
     R[i] = (m1[i][1]*m2[1])+(m1[i][2]*m2[2])+(m1[i][3]*m2[3]);
   end
-  return R;
+  return R
 end
 --#endregion
 
@@ -142,15 +179,15 @@ end
 function update () -- periodic function that will be called
   --#region START MISSION
   if not mission_loaded then
-    read_mission(navigation_file)
-    mission_loaded = true
+    read_mission(navigation_file);
+    mission_loaded = true;
   end
   -- start mission
   if (not mission_started and not arming:is_armed()) then
-    gcs:send_text(6, 'Arming')
-    arming:arm()
+    gcs:send_text(6, 'Arming');
+    arming:arm();
   elseif (arming:is_armed() and vehicle:get_mode() == 0 and repeat_mission ) then
-    gcs:send_text(6, 'Mode auto')
+    gcs:send_text(6, 'Mode auto');
     if not vehicle:set_mode(3) then
       return update, 10000
     end
@@ -160,12 +197,14 @@ function update () -- periodic function that will be called
 
   if mission:state() == mission.MISSION_RUNNING then -- check to see if mission is running
     if (mission_ended == true) then
-      mission_ended = false
-      mission_started = true
+      mission_ended = false;
+      mission_started = true;;
+      vetor_erro = {0,0,0};
       verror_size = 0;
+      first_position = ahrs:get_relative_position_NED_home();
       replication_number = replication_number+1; --flag for file change increments
-      gcs:send_text(6, 'Replication number ' .. replication_number)
-      written_file_replication = false
+      gcs:send_text(6, 'Replication number ' .. replication_number);
+      written_file_replication = false;
       write_to_file(all_errors_file, {replication_number});
     end
 
@@ -175,8 +214,9 @@ function update () -- periodic function that will be called
 
     --#region P WI WI+1 DEFINITION
     if mission:get_current_nav_index() == 2 then
-      wi = (ahrs:get_home()):get_vector_from_origin_NEU(); --the first waypoint is home position
-      wi:x(-wi:x());wi:y(-wi:y());wi:z(-10); -- direction change beacause of conversion
+      wi = first_position; --the first waypoint where airship is currently situated
+      --(ahrs:get_home()):get_vector_from_origin_NEU(); 
+      wi:z(-10); -- we only measure on air
 
     elseif wp ~= mission:get_current_nav_index()  then
       wi = wi1; -- otherwise wi is the previous wi+1
@@ -230,7 +270,7 @@ function update () -- periodic function that will be called
 
     --#region EIGTH STEP: Ry' * Rz * P - WI
     local erro = MvV(Ry,(MvV(Rz,pminusw)));
-    vetor_erro = {vetor_erro[1]+erro[1],vetor_erro[2]+erro[2],vetor_erro[3]+erro[3]};
+    vetor_erro = {vetor_erro[1]+math.abs(erro[1]),vetor_erro[2]+math.abs(erro[2]),vetor_erro[3]+math.abs(erro[3])};
     verror_size = verror_size+1;
     --#endregion
 
@@ -256,23 +296,23 @@ function update () -- periodic function that will be called
       vetor_erro = {vetor_erro[1]/verror_size,vetor_erro[2]/verror_size,vetor_erro[3]/verror_size};
       local csv_data = {replication_number, vetor_erro[1], vetor_erro[2], vetor_erro[3]};
       write_to_file(mean_file, csv_data);
-      written_file_replication = true
+      written_file_replication = true;
     end
     if (replication_number == max_number_replications) then
       repeat_mission = false
     end
 
     if repeat_mission then
-      mission:set_current_cmd(0)
-      mission_started = false
-      vehicle:set_mode(0)
+      mission:set_current_cmd(0);
+      mission_started = false;
+      vehicle:set_mode(0);
     end
-    mission_ended = true
+    mission_ended = true;
 
   end
   return update, 500 -- request "update" to be rerun again 1000 milliseconds (1/2 second) from now
 end
 
-mission_param() --call to create as soon as the script starts
-experiment_control() --call to create as soon as the script starts
+mission_param(); --call to create as soon as the script starts
+experiment_control(); --call to create as soon as the script starts
 return update, 500   -- request "update" to be the first time 1000 milliseconds (1/2 second) after script is loaded
